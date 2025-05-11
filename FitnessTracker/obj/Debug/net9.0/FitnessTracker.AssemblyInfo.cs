713d98a556541e190eef3ceb459126eb7f1ff7f3
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-05-11 20:53:24 +0100

    Added gitignore.

--- FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde7b7d45830a132ba4138b670d0c744ec65f40f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
