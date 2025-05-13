3fb2bd82fd5d19d3ff2267d41a7bfeedd15430a7
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-05-13 19:25:27 +0100

    Added inline editing to datagrid, buttons not responding yet. Needs debugging.

--- FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b684cee292838e542478e17011b553ea09d5327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f06249e8a23a89cee943051dcafb10a34ddc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
