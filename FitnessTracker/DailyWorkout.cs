9e49309028a58004eb1316d4070459cd761963b5
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-05-15 19:10:57 +0100

    Made database changes, scaffolded to project, lots of errors to fix.
    DON'T FORGET to add back the foods_in_meal table before fixing the errors in project. :)

---
+++ FitnessTracker/DailyWorkout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessTracker;
+
+public partial class DailyWorkout
+{
+    public int DworkoutId { get; set; }
+
+    public int LogId { get; set; }
+
+    public bool PreWorkout { get; set; }
+
+    public string? Notes { get; set; }
+
+    public virtual DailyLog Log { get; set; } = null!;
+}
---
+++ FitnessTracker/FoodMealInLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessTracker;
+
+public partial class FoodMealInLog
+{
+    public int? LogId { get; set; }
+
+    public int? FoodId { get; set; }
+
+    public int? MealId { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public virtual Food? Food { get; set; }
+
+    public virtual DailyLog? Log { get; set; }
+
+    public virtual Meal? Meal { get; set; }
+}
