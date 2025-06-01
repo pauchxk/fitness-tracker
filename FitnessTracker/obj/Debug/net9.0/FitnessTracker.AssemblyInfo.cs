0f16760128c007e9eba1d3516184dd515c5789f1
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-06-01 15:19:53 +0100

    Added datagrids for all daily meals, fixed bug with amount in dailymealform.

--- FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df08c6cbf61792438bbd2a2e3661de5c9da6842")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c0d104db65b095070835f02a77e8bceb759c26")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
