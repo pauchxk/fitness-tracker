f4d70c0ba8d1432efd8cbb4298cccf18abac8f45
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-06-01 14:32:58 +0100

    Fixed nullreference exception error with generating daily logs.

--- FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872b159f0a6ac81cd11e99c5adf348148830f7ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17dff53f8c81118432fb77c5a9e33bc01cd25a61")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
