8987171975456841a77cd1f2a61c23a31b46b043
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-06-01 20:22:21 +0100

    Added backend objects and functions for adding meals.

--- FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c0d104db65b095070835f02a77e8bceb759c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f16760128c007e9eba1d3516184dd515c5789f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
