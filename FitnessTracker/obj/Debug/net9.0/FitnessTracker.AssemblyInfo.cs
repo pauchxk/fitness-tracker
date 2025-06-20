af72bfde45ef22218dc750cac70c9b654717f5b9
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-06-20 13:03:50 +0100

    Added date navigation to daily nutrition page.

--- FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
+++ FitnessTracker/obj/Debug/net9.0/FitnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad6d742cfd2aa8e4d11a91a9db192810aa8b8bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52e5162971c5534d990ae9492e2110a5934ef96")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
